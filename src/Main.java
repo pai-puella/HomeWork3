ff7f025d1058cbc789a64e4cfa924f3ef5c3eff1
pai-puella <cat.dyshkant@yandex.ru>
2023-09-13 18:30:13 +0300

    Added task 1

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задание 1");
+        System.out.println("Задача 1");
 
+        int myInt = 11;
+        byte myByte = 5;
+        short myShort = 100;
+        long myLong = 1000000000L;
+        float myFloat = 3.14f;
+        double myDouble = 2.71645;
+
+        System.out.println("Значение переменной myInt c типом int равно " + myInt);
+        System.out.println("Значение переменной myByte c типом byte равно " + myByte);
+        System.out.println("Значение переменной myShort c типом short равно " + myShort);
+        System.out.println("Значение переменной myLong c типом long равно " + myLong);
+        System.out.println("Значение переменной myFloat c типом float равно " + myFloat);
+        System.out.println("Значение переменной myDouble c типом double равно " + myDouble);
 
     }
 }
